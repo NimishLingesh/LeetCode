ddcb821a30c3b98f328bfa544d4ca0a3ad994dbc
Spartan <nimish.lingesh@gmail.com>
2022-09-07 19:46:00 -0700

    3_to_7_sept

--- 394_decode_str.py
+++ 394_decode_str.py
@@ -3,8 +3,11 @@ class Solution:
         while len(s) != 0:
             num = s.pop(0)
             num = int(num)
-            
+
+
+48-57
 
 sol = Solution()
 s = "3[a]2[bc]"
+# s = "3[a2[c]]"
 print(sol.decodeString(s))
